6c4bf85874019125c10cb7bdb1f47c9c81855645
Atik <luice243@gmail.com>
2016-10-01 03:03:59 +0600

    before cart

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+@section('title')
+
+Shopping cart
+@endsection
+
+@section('content')
+    @foreach ($product->chunk(3) as $chunk)
+        <div class="row">
+            @foreach ($chunk as $products)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{$products->imgpath}}" alt="...">
+                        <div class="caption">
+                            <h3>{{$products->title}}</h3>
+                            <p class="description">{{$products->description}}</p>
+                            <div class="price pull-left">{{$products->price}}</div>
+                            <div class="clearfix"><a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+
+
+
+
+
+
+
+
+@stop
